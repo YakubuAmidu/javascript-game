addb98dbbc556cd1636bae09f9ce9984c1fbafbd
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2023-06-19 08:34:46 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ravens = [];
+
+class Raven {
+    constructor(){
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * canvas.height - this.height;
+    }
+}
