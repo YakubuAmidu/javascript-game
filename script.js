5a93e218af09386a20f8778c5b435015dbc1519a
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-22 08:11:45 -0700

    Initial commit

--- script.js
+++ script.js
@@ -99,6 +99,26 @@ class Explosion {
     }
 }
 
+let particles = [];
+
+class Particles {
+    constructor(x, y, size, color){
+        this.x = x;
+        this.y = y;
+        this.radius = Math.random() * this.size / 10;
+        this.maxRadius = Math.random() * 20 + 35;
+        this.markedForDeletion = false;
+        this.speedX = Math.random() * 1 + 0.5;
+        this.color = color;
+    }
+
+    update(){
+        this.x += this.speedX;
+        this.radius += 0.2;
+        if(this.radius > this.maxRadius) this.markedForDeletion = true;
+    }
+}
+
 function drawScore(){
     ctx.fillStyle = 'black';
     ctx.fillText('Score: ' + score, 50, 75);
