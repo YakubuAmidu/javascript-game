e9ccf9a576935f8f1febcc2c462a6e1d98c36e37
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-24 08:15:31 -0700

    Initial commit

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ class Raven {
         this.flapInterval = Math.random() * 50 + 50;
         this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
         this.colors = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
+        this.hasTrail = Math.random() > 0.5;
     };
 
     update(deltatime){
@@ -54,7 +55,11 @@ class Raven {
             if(this.frame > this.maxFrame) this.frame = 0;
             else this.frame++;
             this.timeSinceFlap = 0;
-            particles.push(new Particle(this.x, this.y, this.width, this.color));
+            if(this.hasTrail){
+                for (let i = 0; i < 5; i++){
+                  particles.push(new Particle(this.x, this.y, this.width, this.color));
+                }
+            }
         }
         if(this.x < 0 - this.width) gameOver = true;
     };
@@ -117,14 +122,17 @@ class Particle {
     update(){
         this.x += this.speedX;
         this.radius += 0.5;
-        if(this.radius > this.maxRadius) this.markedForDeletion = true;
+        if(this.radius > this.maxRadius - 5) this.markedForDeletion = true;
     }
 
     draw(){
+        ctx.save();
+        ctx.globalAlpha = 1 - this.radius / this.maxRadius;
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
+        ctx.restore();
     }
 }
 
